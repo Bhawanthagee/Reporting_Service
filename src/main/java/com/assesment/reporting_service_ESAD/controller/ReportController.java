e1abcf9b84c55c64d5c2484e6639a8177d9c367b
Bhawantha <geeganagebawantha@gmail.com>
2024-10-17 16:29:14 +0530

    initial test commit

--- src/main/java/com/assesment/reporting_service_ESAD/controller/ReportController.java
+++ src/main/java/com/assesment/reporting_service_ESAD/controller/ReportController.java
@@ -1,4 +1,17 @@
 package com.assesment.reporting_service_ESAD.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/reports")
 public class ReportController {
+    @GetMapping("/test")
+    public ResponseEntity<String> testController() {
+        String message = "Reporting service is up and running!";
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
 }
